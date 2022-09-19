cab225251e475d01cb5a9c711afdfddec957456c
FluSett <flusett@gmail.com>
2022-09-19 20:38:05 +0300

    add the generated routing

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,31 +3,36 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:i18n_extension/i18n_widget.dart';
 
+import 'core/constants/route_paths.dart' as routes;
 import 'core/cubit/app_colors_cubit.dart';
 import 'core/dependency_injection.dart';
+import 'core/router.dart' as router;
+import 'core/service/navigation_service.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => getIt<AppColorsCubit>(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Exminses',
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          Locale('en', 'US'),
-          Locale('uk', 'UA'),
-          Locale('ru', 'RU'),
-        ],
-        home: I18n(
-          child: const Scaffold(),
+    return I18n(
+      child: BlocProvider(
+        create: (_) => getIt<AppColorsCubit>(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Exminses',
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en', 'US'),
+            Locale('uk', 'UA'),
+            Locale('ru', 'RU'),
+          ],
+          navigatorKey: getIt<NavigationService>().navigatorKey,
+          onGenerateRoute: router.generateRoute,
+          initialRoute: routes.tempForTest,
         ),
       ),
     );
