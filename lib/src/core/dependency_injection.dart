b4733bb19606b5888f06b36335298ef7a58257fb
FluSett <flusett@gmail.com>
2022-09-19 20:37:18 +0300

    register a singleton for the navigation service

--- lib/src/core/dependency_injection.dart
+++ lib/src/core/dependency_injection.dart
@@ -1,9 +1,11 @@
 import 'package:get_it/get_it.dart';
 
 import 'cubit/app_colors_cubit.dart';
+import 'service/navigation_service.dart';
 
 final getIt = GetIt.instance;
 
 void setUpDI() {
   getIt.registerLazySingleton<AppColorsCubit>(() => AppColorsCubit());
+  getIt.registerLazySingleton<NavigationService>(() => NavigationService());
 }
