815dcd420145e4912bf220a5c960f3ea16627e09
FluSett <flusett@gmail.com>
2022-09-27 00:55:33 +0300

    Auth listener & AppColors Theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:i18n_extension/i18n_widget.dart';
 
+import 'core/constants/locales.dart';
 import 'core/constants/route_paths.dart' as routes;
 import 'core/cubit/app_colors_cubit.dart';
 import 'core/dependency_injection.dart';
+import 'core/model/app_colors.dart';
 import 'core/router.dart' as router;
 import 'core/service/navigation_service.dart';
 import 'features/authentication/bloc/authentication_bloc.dart';
@@ -15,6 +18,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final navigationService = getIt<NavigationService>();
     return I18n(
       child: MultiBlocProvider(
         providers: [
@@ -26,22 +30,41 @@ class App extends StatelessWidget {
               ..add(const AuthenticationEvent.startCheckAuthentication()),
           ),
         ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Exminses',
-          localizationsDelegates: const [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en', 'US'),
-            Locale('uk', 'UA'),
-            Locale('ru', 'RU'),
-          ],
-          navigatorKey: getIt<NavigationService>().navigatorKey,
-          onGenerateRoute: router.generateRoute,
-          initialRoute: routes.splash,
+        child: BlocBuilder<AppColorsCubit, AppColors>(
+          builder: (context, state) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                textTheme: GoogleFonts.ptMonoTextTheme(),
+                scaffoldBackgroundColor: state.scaffold,
+                primaryColor:
+                    state.filledButton, //TODO: change it after fix AppColors
+              ),
+              title: 'Exminses',
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: locales,
+              navigatorKey: navigationService.navigatorKey,
+              onGenerateRoute: router.generateRoute,
+              initialRoute: routes.splash,
+              builder: ((_, child) {
+                return BlocListener<AuthenticationBloc, AuthenticationState>(
+                  listener: (_, state) {
+                    state.mapOrNull(
+                      unauthenticated: (_) => navigationService
+                          .navigateToAndRemoveUntil(routes.welcome),
+                      authenticated: (_) => navigationService
+                          .navigateToAndRemoveUntil(routes.home),
+                    );
+                  },
+                  child: child,
+                );
+              }),
+            );
+          },
         ),
       ),
     );
