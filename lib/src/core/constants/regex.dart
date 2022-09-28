712e6a8e3b473941350419dc46033d56b0943d9f
FluSett <flusett@gmail.com>
2022-09-28 03:02:32 +0300

    Constants regex patterns

---
+++ lib/src/core/constants/regex.dart
@@ -0,0 +1,5 @@
+//original - (^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)
+const email = '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$)';
+
+//original - ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$
+const password = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,16}\$';
